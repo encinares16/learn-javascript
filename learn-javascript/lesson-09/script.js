353520cc015254932599c6fc5a4a19bc115856eb
Jerome Encinares Talavera <139388162+encinares16@users.noreply.github.com>
2024-01-17 23:50:19 +0800

    Add files via upload

---
+++ learn-javascript/lesson-09/script.js
@@ -0,0 +1,16 @@
+
+//  JS Array
+
+let colors = ['blue', 'red', 'green']
+
+// array methods
+colors.push("orange")      // add an element
+colors.pop()               // remove last element
+colors.unshift("violet")   // add element to first
+colors.shift()             // removes element to first
+
+let length = colors.length
+let index = colors.indexOf("blue")
+
+console.log(length)
+console.log(index)
